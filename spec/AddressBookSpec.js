4545e7ee437c1d2e50c3a58ee3925849d8014a6b
Baochuan Lu <blu@sbuniv.edu>
2017-09-09 04:03:47 +0000

    use beforeEach() to simlify tests

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,8 +1,12 @@
 describe('Address Book', function() {
-	it('should be able to add a new contact', function() {
-		var addressBook = new AddressBook(),
-		    thisContact = new Contact();
+	var addressBook, thisContact;
 
+	beforeEach(function() {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
+
+	it('should be able to add a new contact', function() {
 		addressBook.addContact(thisContact);
 
 		expect(addressBook.getContact(0)).toBe(thisContact);
